6a38265777c6a10d891cd02a102f3c5984d6d00e
Dan Smout <dj.smout@gmail.com>
2021-06-18 23:58:05 +0100

    Initial Commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    // println!("{:?}", args);
+    for i in args
+    {
+        println!("{}",i);
+    }
+
+    // println!("Hello, world!");
 }
